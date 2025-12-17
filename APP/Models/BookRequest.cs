dc83b8f043d7f3bb60e989fc80a4d0e579fab3a6
encanmore <elifcandan0625@gmail.com>
2025-12-17 23:54:57 +0300

    created bookrequest and bookresponse

---
+++ APP/Models/BookRequest.cs
@@ -0,0 +1,22 @@
+﻿using APP.Domain;
+using CORE.APP.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace APP.Models
+{
+    public class BookRequest : Request
+    {
+        [Required(ErrorMessage = "{0} is required!")]
+        [StringLength(30, ErrorMessage = "{0} must be maximum {1} characters!")]
+        public string Name { get; set; }
+        public short? NumberOfPages { get; set; }
+        public DateTime PublishDate { get; set; }
+        [Required(ErrorMessage = "{0} is required!")]
+        public decimal Price { get; set; }
+        [DisplayName("Top Seller?")]
+        public bool IsTopSeller { get; set; }
+        [DisplayName("Author's ID")]
+        public int AuthorId { get; set; }
+    }
+}
---
+++ APP/Models/BookResponse.cs
@@ -0,0 +1,24 @@
+﻿using APP.Domain;
+using CORE.APP.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace APP.Models
+{
+    public class BookResponse : Response
+    {
+        public string Name { get; set; }
+        public short? NumberOfPages { get; set; }
+        public DateTime PublishDate { get; set; }
+        public decimal Price { get; set; }
+        public bool IsTopSeller { get; set; }
+        public int AuthorId { get; set; }
+
+        [DisplayName("Publish Date")]
+        public string PublishDateF { get; set; }
+        public string PriceF { get; set; }
+        [DisplayName("Top Seller?")]
+        public string IsTopSellerF { get; set; }
+
+    }
+}
