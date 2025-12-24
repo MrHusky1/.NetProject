9e1375a924ab4d75101d43f751a3ae3868212f8a
MrHusky1 <alper.dinler13@gmail.com>
2025-12-24 11:19:16 +0300

    bookrequest display names added.

--- APP/Models/BookRequest.cs
+++ APP/Models/BookRequest.cs
@@ -7,16 +7,25 @@ namespace APP.Models
 {
     public class BookRequest : Request
     {
+        [DisplayName("Book Name")]
         [Required(ErrorMessage = "{0} is required!")]
         [StringLength(30, ErrorMessage = "{0} must be maximum {1} characters!")]
         public string Name { get; set; }
+
+        [DisplayName("Number of Pages")]
         public short? NumberOfPages { get; set; }
+
+        [DisplayName("Publish Date")]
         public DateTime? PublishDate { get; set; }
+
+        [DisplayName("Price")]
         [Required(ErrorMessage = "{0} is required!")]
         public decimal? Price { get; set; }
-        [DisplayName("Top Seller?")]
+
+        [DisplayName("Is Top Seller?")]
         public bool IsTopSeller { get; set; }
-        [DisplayName("Author's ID")]
+
+        [DisplayName("Author")] // Simplified from Author's ID for better UI
         [Required(ErrorMessage = "{0} is required!")]
         public int? AuthorId { get; set; }
 
